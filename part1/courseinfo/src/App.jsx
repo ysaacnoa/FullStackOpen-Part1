97d98c08468b2e5a5d352bc5b0208c54c16ffb43
LilAtza <ysaacnoe.correa@unmsm.edu.pe>
2023-12-15 21:05:49 -0500

    courseInfo

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,69 @@
+
+const Header = ({course}) =>{
+  return(
+    <>
+      <h1>{course.name}</h1>
+    </>
+  )
+}
+
+const Content = ({course}) =>{
+  return(
+    <>
+      <Part part={course.parts[0]} />
+      <Part part={course.parts[1]} />
+      <Part part={course.parts[2]} />
+    </>
+  )
+}
+
+const Part = ({part}) =>{
+  return(
+  <>
+    <p>{part.name} {part.exercises}</p>
+  </>
+  )
+}
+
+const Total = ({course}) => {
+  return (
+    <>
+      <p>Number of exercises { 
+      course.parts[0].exercises+course.parts[1].exercises+course.parts[2].exercises
+      }
+      </p>
+    </>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content course={course}/>
+
+      <Total course={course}/>
+      {console.log('courseInfo Terminado con Exito :D!')}
+    </div>
+  )
+}
+
+export default App
